f65e7d42633f1a183dd83fd29bf9a885f2cc9be8
Emir Sarikus <0948103@hr.nl>
2021-05-08 16:12:22 +0200

    added cool effects

--- portfolio/js/main.js
+++ portfolio/js/main.js
@@ -1,3 +1,4 @@
+//navbar toggle
 $(function () {
   $(".toggle").on("click", function () {
     if ($(".item").hasClass("active")) {
@@ -6,4 +7,22 @@ $(function () {
       $(".item").addClass("active");
     }
   });
+});
+
+//scroll down to about section
+
+$(document).ready(function () {
+  $(".btn-readmore").click(function () {
+    $("html").animate(
+      {
+        scrollTop: $(".about").offset().top,
+      },
+      800 //speed
+    );
+  });
+});
+
+//pre-loader
+$(window).on("load", function () {
+  $(".loader-container").fadeOut(1000);
 });
